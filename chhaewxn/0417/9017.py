61204ef6ad91299a2338d71d3f75cd988562eb22
chhaewxn <chaewon1019@ewhain.net>
2024-04-20 23:44:15 +0900

    [9017] 풀이 완료(240420)

--- chhaewxn/0417/9017.py
+++ chhaewxn/0417/9017.py
@@ -5,31 +5,30 @@ import sys
 
 input = int(sys.stdin.readline().strip())
 
-for _ in range (input):
+for _ in range(input):
   n = int(sys.stdin.readline().strip())
   rank = list(map(int, sys.stdin.readline().split()))
   result = {}
 
-  for data in score:
+  for data in rank:
     result[data] = result.get(data, 0) + 1
 
-  fifth_score = [0] * (max(result.keys()) + 1)
-  score_map = {}
+  fifth_score = [0] * (max(result.keys()) + 1) # 5번째 선수의 점수 저장하는 배열 
+  score_map = {} # 팀의 총 점수 저장
   score = 1
-  temp = {}
+  temp = {} # 팀별로 처리된 선수의 수를 임시로 저장
 
   for element in rank:
     if result[element] >= 6:
       temp[element] = temp.get(element, 0) + 1
 
-    if temp[element] <= 4:
-      score_map[element] = score_map.get(element, 0) + score
+      if temp[element] <= 4:
+        score_map[element] = score_map.get(element, 0) + score
 
-    if temp[element] == 5:
-      fifth_score[element] = score
-    score += 1
-
-key_data = list(score_map.keys())
-key_data.sort(key=lambda x: (score_map[x], fifth_score[x]))
-
-print(key_data[0])
+      if temp[element] == 5:
+        fifth_score[element] = score
+      score += 1
+      
+  key_data = list(score_map.keys())
+  key_data.sort(key=lambda x: (score_map[x], fifth_score[x]))
+  print(key_data[0])
