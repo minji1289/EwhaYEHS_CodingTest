cb57b331cca5ba4e5a25342418a6b95e6dfb97a1
Kim Seohyun <59790777+kshjessica@users.noreply.github.com>
2024-04-24 20:08:32 +0900

    [2607] 풀이 중(240410)

--- kshjessica/백준/2607-비슷한_단어.py
+++ kshjessica/백준/2607-비슷한_단어.py
@@ -15,7 +15,8 @@ for _ in range(n - 1):
             # Remove matching characters from the copy
             compare.remove(w)
         else:
-            cnt += 1  # Increment the difference counter
+            # Increment the difference counter
+            cnt += 1
 
     if cnt < 2 and len(compare) < 2:
         answer += 1  # Increment the answer counter if the words are similar
