47fd80d839d0dfe401a41dd36266ab9d2e42da6f
Kim Seohyun <59790777+kshjessica@users.noreply.github.com>
2024-05-01 11:12:24 +0900

    [1927] 풀이 완료(240501)

--- kshjessica/0501/1927-최소_힙.py
+++ kshjessica/0501/1927-최소_힙.py
@@ -1,10 +1,11 @@
 import heapq
+import sys
 
-n = int(input())
+n = int(sys.stdin.readline())
 heap = []
 
 for _ in range(n):
-    x = int(input())
+    x = int(sys.stdin.readline())
 
     if x != 0:
         heapq.heappush(heap, x)
